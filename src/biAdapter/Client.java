29ad06806a3cac053b78a8d33acb6c990f043859
guohaolian <1040071899@qq.com>
2024-12-23 18:56:31 +0800

    change

--- src/biAdapter/Client.java
+++ src/biAdapter/Client.java
@@ -3,9 +3,12 @@ package biAdapter;
 public class Client {
     public static void main(String[] args) {
         Adapter adapter = new Adapter();
-        adapter.cry();
-        adapter.catchMouse();
-        adapter.bark();
-        adapter.action();
+        Cat cat=(Cat)adapter;
+        Dog dog=(Dog)adapter;
+        cat.cry();
+        cat.catchMouse();
+        dog.bark();
+        dog.action();
+
     }
 }
